3ad7964b36acdfd7c13d22af22aaa33b21104981
nihal <nihaltahariya95@gmail.com>
2024-05-12 13:04:07 +0200

    Modified Readme and segregated the tests for green and blue boxes

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Asaphus_challenge
+
+
+git clone https://github.com/andy1122/Asaphus_challenge.git
+
+mkdir build
+cd build
+cmake -S . -B build
+cmake --build build
+
+
+To check if test case passed
+./build/test/unit_tests 
+
+Randomness seeded to: 2454658080
+===============================================================================
+All tests passed (4 assertions in 4 test cases)
+
+
+Get more comprehensive view of the test case
+cd build
+ctest
+
+    Start 1: Final scores for first 4 Fibonacci numbers
+1/4 Test #1: Final scores for first 4 Fibonacci numbers ...............   Passed    0.01 sec
+    Start 2: Final scores for first 8 Fibonacci numbers
+2/4 Test #2: Final scores for first 8 Fibonacci numbers ...............   Passed    0.01 sec
+    Start 3: Final scores of reverse of  first  4 Fibonacci numbers
+3/4 Test #3: Final scores of reverse of  first  4 Fibonacci numbers ...   Passed    0.01 sec
+    Start 4: Final scores with 4 Random numbers
+4/4 Test #4: Final scores with 4 Random numbers .......................   Passed    0.01 sec
+
+100% tests passed, 0 tests failed out of 4
+
+Total Test time (real) =   0.03 sec
+
--- test/test_game.cpp
+++ test/test_game.cpp
@@ -8,11 +8,6 @@ TEST_CASE("Final scores for first 4 Fibonacci numbers", "[fibonacci4]") {
   auto result = play(inputs, boxes);
   REQUIRE(result.first == 13.0);
   REQUIRE(result.second == 25.0);
-  // check weights 
-  REQUIRE(boxes[0]->getWeight() == 1.0);
-  REQUIRE(boxes[1]->getWeight() == 1.1);
-  REQUIRE(boxes[2]->getWeight() == 2.2);
-  REQUIRE(boxes[3]->getWeight() == 3.3);
 }
 
 TEST_CASE("Final scores for first 8 Fibonacci numbers", "[fibonacci8]") {
@@ -21,11 +16,6 @@ TEST_CASE("Final scores for first 8 Fibonacci numbers", "[fibonacci8]") {
   auto result = play(inputs, boxes);
   REQUIRE(result.first == 155.0);
   REQUIRE(result.second == 366.25);
-  // check weights
-  REQUIRE(boxes[0]->getWeight() == 6.0);
-  REQUIRE(boxes[1]->getWeight() == 9.1);
-  REQUIRE(boxes[2]->getWeight() == 15.2);
-  REQUIRE(boxes[3]->getWeight() == 24.3);
 }
 
 
@@ -35,11 +25,6 @@ TEST_CASE("Final scores of reverse of  first  4 Fibonacci numbers", "[Reverse_fi
   auto result = play(inputs, boxes);
   REQUIRE(result.first == 13.0);
   REQUIRE(result.second == 8.0);
-  // check weights
-  REQUIRE(boxes[0]->getWeight() == 3.0);
-  REQUIRE(boxes[1]->getWeight() == 2.1);
-  REQUIRE(boxes[2]->getWeight() == 1.2);
-  REQUIRE(boxes[3]->getWeight() == 1.3);
 }
 
 TEST_CASE("Final scores with 4 Random numbers", "[Random_4]") {
@@ -48,10 +33,63 @@ TEST_CASE("Final scores with 4 Random numbers", "[Random_4]") {
   auto result = play(inputs, boxes);
   REQUIRE(result.first == 25.0);
   REQUIRE(result.second == 76.0);
-  // check weights
+}
+
+
+TEST_CASE("Test absorption of green box", "[green]") {
+  // TODO
+  std::vector<uint32_t> inputs{1, 4, 3, 5};
+  std::vector<std::unique_ptr<Box>> boxes;
+  auto result = play(inputs, boxes);
   REQUIRE(boxes[0]->getWeight() == 1.0);
   REQUIRE(boxes[1]->getWeight() == 4.1);
+
+  std::vector<uint32_t> inputs1{3, 2, 1, 1};
+  std::vector<std::unique_ptr<Box>> boxes1;
+  auto result1 = play(inputs1, boxes1);
+  REQUIRE(boxes1[0]->getWeight() == 3.0);
+  REQUIRE(boxes1[1]->getWeight() == 2.1);
+
+  std::vector<uint32_t> inputs2{1, 1, 2, 3};
+  std::vector<std::unique_ptr<Box>> boxes2;
+  auto result2 = play(inputs2, boxes2);
+  REQUIRE(boxes2[0]->getWeight() == 1.0);
+  REQUIRE(boxes2[1]->getWeight() == 1.1);
+
+  std::vector<uint32_t> inputs3{1, 1, 2, 3, 5, 8, 13, 21};
+  std::vector<std::unique_ptr<Box>> boxes3;
+  auto result3 = play(inputs3, boxes3);
+  REQUIRE(boxes3[0]->getWeight() == 6.0);
+  REQUIRE(boxes3[1]->getWeight() == 9.1);
+ 
+}
+
+TEST_CASE("Test absorption of blue box", "[blue]") {
+  // TODO
+    std::vector<uint32_t> inputs{1, 4, 3, 5};
+  std::vector<std::unique_ptr<Box>> boxes;
+  auto result = play(inputs, boxes);
   REQUIRE(boxes[2]->getWeight() == 3.2);
   REQUIRE(boxes[3]->getWeight() == 5.3);
+
+  std::vector<uint32_t> inputs1{3, 2, 1, 1};
+  std::vector<std::unique_ptr<Box>> boxes1;
+  auto result1 = play(inputs1, boxes1);
+  REQUIRE(boxes1[2]->getWeight() == 1.2);
+  REQUIRE(boxes1[3]->getWeight() == 1.3);
+
+  std::vector<uint32_t> inputs2{1, 1, 2, 3};
+  std::vector<std::unique_ptr<Box>> boxes2;
+  auto result2 = play(inputs2, boxes2);
+  REQUIRE(boxes2[2]->getWeight() == 2.2);
+  REQUIRE(boxes2[3]->getWeight() == 3.3);
+
+  std::vector<uint32_t> inputs3{1, 1, 2, 3, 5, 8, 13, 21};
+  std::vector<std::unique_ptr<Box>> boxes3;
+  auto result3 = play(inputs3, boxes3);
+  REQUIRE(boxes3[2]->getWeight() == 15.2);
+  REQUIRE(boxes3[3]->getWeight() == 24.3);
+
 }
+
 
