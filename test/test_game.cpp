0f502023e0f30e931ec11bfad2c4fe7de395ea46
nihal <nihaltahariya95@gmail.com>
2024-05-12 11:54:57 +0200

    Add 2 test cases

--- test/test_game.cpp
+++ test/test_game.cpp
@@ -16,6 +16,21 @@ TEST_CASE("Final scores for first 8 Fibonacci numbers", "[fibonacci8]") {
   REQUIRE(result.second == 366.25);
 }
 
+
+TEST_CASE("Final scores of reverse of  first  4 Fibonacci numbers", "[Reverse_fibonacci4]") {
+  std::vector<uint32_t> inputs{3, 2, 1, 1};
+  auto result = play(inputs);
+  REQUIRE(result.first == 13.0);
+  REQUIRE(result.second == 8.0);
+}
+
+TEST_CASE("Final scores with 4 Random numbers", "[Random_4]") {
+  std::vector<uint32_t> inputs{1, 4, 3, 5};
+  auto result = play(inputs);
+  REQUIRE(result.first == 25.0);
+  REQUIRE(result.second == 76.0);
+}
+
 TEST_CASE("Test absorption of green box", "[green]") {
   // TODO
 }
